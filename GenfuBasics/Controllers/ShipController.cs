bd96399903fa95a0bb58c3197ed7abd8c5df4a4e
bugra4569 <bugraocalan@gmail.com>
2021-02-06 21:50:24 +0300

    Add project files.

---
+++ GenfuBasics/Controllers/ShipController.cs
@@ -0,0 +1,31 @@
+﻿using GenFu;
+using GenfuBasics.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenfuBasics.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShipController : ControllerBase
+    {
+        [HttpGet("GetAllShips")]
+        public IActionResult GetAllShips()
+        {
+            
+            var fakeId = 1;
+            A.Configure<Ships>()
+                .Fill(x => x.GrossTonnage).WithinRange(10, 100)
+                .Fill(x => x.Id, () => { return fakeId++; })
+                .Fill(x => x.TypeOf).WithRandom(new string[] { "Container", "Bulk", "Tanker", "Naval", "OffShore", "Special Purpose" })
+                .Fill(x => x.ImoNumber).WithinRange(100000, 999999);
+           
+            return Ok(A.ListOf<Ships>(100));
+
+        }
+    }
+}
---
+++ GenfuBasics/Entities/Ships.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenfuBasics.Entities
+{
+    public class Ships
+    {
+
+     
+        public int Id { get; set; }
+        public int ShipNumber { get; set; }
+        public int ImoNumber { get; set; }
+        public string ShipName { get; set; }
+        public decimal RegisteredLength { get; set; }
+        public string TypeOf { get; set; }
+        public decimal GrossTonnage { get; set; }
+        public decimal NetTonnage { get; set; }
+      
+
+
+    }
+}
